321312f5aaddd9d49287d2bee40b035dea640168
Leandro Freire <88214669+lendrw@users.noreply.github.com>
2025-07-20 16:08:02 -0300

    .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './features/auth/auth.guard';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'courses', pathMatch: 'full' },
   {
     path: 'courses',
-    // Students will implement loadChildren here
+    loadChildren: () =>
+      import('./features/courses/courses.module').then((m) => m.CoursesModule),
+    canActivate: [AuthGuard],
   },
-  {
-    path: 'auth',
-    // Students will implement loadChildren here
-  },
-  // Students will implement basic routes here
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { } 
